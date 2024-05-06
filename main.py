f64e7aac61d5bacb52963f5ed896e04c67463ae6
MrR3353 <mishinstepan2000@gmail.com>
2024-05-07 01:05:00 +0300

    Сайт для хранения репозиториев

---
+++ main.py
@@ -0,0 +1,58 @@
+# .
+# ├── account
+# │   ├── static
+# │   │   └── css
+# │   │       └── base.css
+# │   ├── templates
+# │   │   ├── account
+# │   │   │   └── dashboard.html
+# │   │   ├── registration
+# │   │   │   ├── logged_out.html
+# │   │   │   ├── login.html
+# │   │   │   ├── password_change_done.html
+# │   │   │   ├── password_change_form.html
+# │   │   │   ├── password_reset_complete.html
+# │   │   │   ├── password_reset_confirm.html
+# │   │   │   ├── password_reset_done.html
+# │   │   │   ├── password_reset_email.html
+# │   │   │   └── password_reset_form.html
+# │   │   └── base.html
+# │   ├── admin.py
+# │   ├── apps.py
+# │   ├── forms.py
+# │   ├── __init__.py
+# │   ├── models.py
+# │   ├── tests.py
+# │   ├── urls.py
+# │   └── views.py
+# ├── bookmarks
+# │   ├── asgi.py
+# │   ├── __init__.py
+# │   ├── settings.py
+# │   ├── urls.py
+# │   └── wsgi.py
+# ├── db.sqlite3
+# ├── manage.py
+# └── README.md
+
+# циклы
+
+# print('hello')
+# print('hello')
+# print('hello')
+
+
+# for i in range(5):
+#     print(i)
+
+# for i in range(1, 11):  # [a, b)
+#     print(i)
+
+# for i in range(10, 0, -1):  # [a, b)
+#     print(i)
+
+
+
+
+
+
